fce8fc41d39810fb03d7ec6fe1ed1590f3d8a48b
JhojanV70 <jhojanv70@gmail.com>
2024-05-21 16:48:43 -0500

    Index Horarios

---
+++ Pages/Horarios/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using GestionCecepWEB.Data;
+using GestionCecepWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionCecepWEB.Pages.Horarios
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GestionCecepContext _context;
+        public IndexModel(GestionCecepContext context)
+        {
+            _context = context;
+        }
+        public IList<Horario> Horarios { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Horarios != null)
+            {
+                Horarios = await _context.Horarios.ToListAsync();
+            }
+        }
+    }
+}
