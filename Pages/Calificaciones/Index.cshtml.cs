b14875d5477b48adc3662019577dc538f953d601
JhojanV70 <jhojanv70@gmail.com>
2024-05-22 19:28:47 -0500

    Index Calificaciones

---
+++ Pages/Calificaciones/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using GestionCecepWEB.Data;
+using GestionCecepWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionCecepWEB.Pages.Calificaciones
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GestionCecepContext _context;
+        public IndexModel(GestionCecepContext context)
+        {
+            _context = context;
+        }
+        public IList<Calificacion> Calificaciones { get; set; } = default!;
+        public async Task OnGetAsync()
+        {
+            if (_context.Calificaciones != null)
+            {
+                Calificaciones = await _context.Calificaciones.ToListAsync();
+            }
+        }
+    }
+}
